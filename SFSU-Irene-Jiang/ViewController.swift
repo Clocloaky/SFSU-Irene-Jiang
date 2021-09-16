108a63e6ab3b1fb86d9979acb541e352fc47657a
Irene <ijiang200292@hotmail.com>
2021-09-16 16:56:45 -0700

    finished hwk
    items on ui:
    text view for quote
    label for author name
    button to display next quote
    i used a switch statement again instead of if else

--- SFSU-Irene-Jiang/ViewController.swift
+++ SFSU-Irene-Jiang/ViewController.swift
@@ -10,22 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var messageDisplayOrderPrice: UITextField!
-    
+    var quoteNumber = 0
+    @IBOutlet weak var quoteDisplayView: UITextView!
+    @IBOutlet weak var quoteAuthorView: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        messageDisplayOrderPrice.text="Show Food Price Here"
+
     }
     
 
-    @IBAction func buttonOrderPizza(_ sender: UIButton) {
-        messageDisplayOrderPrice.text="🍕 Price: $19.99"
+    @IBAction func nextQuoteClick(_ sender: UIButton) {
+        quoteNumber += 1
+        
+        switch quoteNumber {
+        case 1:
+            quoteDisplayView.text="Don't watch the clock; do what it does. Keep going."
+            quoteAuthorView.text="Sam Levenson"
+        case 2:
+            quoteDisplayView.text="Quality is not an act, it is a habit."
+            quoteAuthorView.text="Aristotle"
+        case 3:
+            quoteDisplayView.text="Be kind whenever possible. It is always possible."
+            quoteAuthorView.text="Dalai Lama"
+        case 4:
+            quoteDisplayView.text="Never gonna give you up, Never gonna let you down"
+            quoteAuthorView.text="Rick Astley"
+        case 5:
+            quoteDisplayView.text="Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence."
+            quoteAuthorView.text="Helen Keller"
+        default:
+            quoteNumber = 0
+        }
+        
     }
     
-    @IBAction func buttonOrderBurger(_ sender: UIButton) {
-        messageDisplayOrderPrice.text="🍔 Price: $15.99"
-    }
 }
 
