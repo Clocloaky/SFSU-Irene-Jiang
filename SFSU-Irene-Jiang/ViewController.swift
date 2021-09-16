140c170f1573e699f06a5522441f484f9d900328
Irene <ijiang200292@hotmail.com>
2021-09-16 16:13:35 -0700

    finished food image scroller activity
    i created a view and placed an image view, a label and button into the view
    i used a counter to scroll through the images and then used a case statement to indicate the prices of the food

--- SFSU-Irene-Jiang/ViewController.swift
+++ SFSU-Irene-Jiang/ViewController.swift
@@ -10,22 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var messageDisplayOrderPrice: UITextField!
-    
+    var imageNumber = 0
+    @IBOutlet weak var foodDisplayView: UIImageView!
+    @IBOutlet weak var foodPriceView: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        messageDisplayOrderPrice.text="Show Food Price Here"
-    }
-    
 
-    @IBAction func buttonOrderPizza(_ sender: UIButton) {
-        messageDisplayOrderPrice.text="🍕 Price: $19.99"
     }
     
-    @IBAction func buttonOrderBurger(_ sender: UIButton) {
-        messageDisplayOrderPrice.text="🍔 Price: $15.99"
+    
+    @IBAction func nextDishClick(_ sender: UIButton) {
+        imageNumber += 1
+        foodDisplayView.image = UIImage(named: "dish\(imageNumber)")
+
+        if imageNumber == 7 {imageNumber = 1}
+        
+        switch imageNumber {
+        case 1:
+            foodPriceView.text = "Duck: $19.99"
+        case 2:
+            foodPriceView.text = "Egg Tart: $8.99"
+        case 3:
+            foodPriceView.text = "Burger: $15.99"
+        case 4:
+            foodPriceView.text = "Ice Cream: $9.99"
+        case 5:
+            foodPriceView.text = "Lo Mien: $7.99"
+        case 6:
+            foodPriceView.text = "Pizza: $19.99"
+        case 7:
+            foodPriceView.text = "Salad: $10.99"
+        default:
+            print("default should not proc")
+        }
+        
     }
+    
 }
 
