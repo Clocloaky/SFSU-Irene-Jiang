3a10aafb8fb921273d94f4998499e951e56fb268
Irene <ijiang200292@hotmail.com>
2021-10-04 00:58:00 -0700

    created frame for the application
    I think I need to update so I can use vstack to make drop down lists. I wanted to try to do that for my food instead of typing in the names.

--- SFSU-Irene-Jiang/ViewController.swift
+++ SFSU-Irene-Jiang/ViewController.swift
@@ -11,22 +11,27 @@ import UIKit
 
 class ViewController: UIViewController{
     
-    var imageNumber = 0
     var foodName = ["Duck", "Egg Tart", "Burger", "Ice Cream", "Lo Mien", "Pizza", "Salad"]
     var foodPrice: [Double] = [19.99, 8.99, 15.99, 9.99, 7.99, 19.99, 10.99]
-    var orderList = [] as [Double]
-    var orderTotal = 0.0
-    var orderTotalWithTip = 0.0
+    struct Food{
+        var foodName: String
+        var foodPrice: Double
+    }
+    var duck = Food(foodName: "Duck", foodPrice: 19.99)
+    var eggT = Food(foodName: "Egg Tart", foodPrice: 8.99)
+    var burger = Food(foodName: "Burger", foodPrice: 15.99)
+    var iceC = Food(foodName: "Ice Cream", foodPrice: 9.99)
+    var LoM = Food(foodName: "Lo Mien", foodPrice: 7.99)
+    var pizza = Food(foodName: "Pizza", foodPrice: 19.99)
+    var salad = Food(foodName: "Salad", foodPrice: 10.99)
     
-    @IBOutlet weak var foodPictureView: UIImageView!
+    @IBOutlet weak var foodSearchInputField: UITextField!
+    @IBOutlet weak var OrderDishButtonPressed: UIButton!
     @IBOutlet weak var foodPriceDisplayLabel: UILabel!
-    @IBOutlet weak var totalPriceLabel: UILabel!
-    @IBOutlet weak var tipInputField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        totalPriceLabel.text = "$0.00"
-        foodPriceDisplayLabel.text = "$0.00"
+        OrderDishButtonPressed.layer.cornerRadius = 10
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -36,41 +41,7 @@ class ViewController: UIViewController{
     override func viewDidAppear(_ animated: Bool) {
         navigationController?.navigationBar.barStyle = .black
     }
-    
-    @IBAction func nextDishButton(_ sender: UIButton) {
-        if imageNumber >= 6 {imageNumber = 0}
-        foodPictureView.image = UIImage(named: foodName[imageNumber])
-        let foodPriceString = String(foodPrice[imageNumber])
-        foodPriceDisplayLabel.text = foodName[imageNumber] + ": $" + foodPriceString
-        imageNumber += 1
-        
-    }
-    @IBAction func addDishToOrderButton(_ sender: UIButton) {
-        if foodPictureView.image != nil{
-            orderList.append(foodPrice[imageNumber-1])
-            orderTotal = orderTotal + foodPrice[imageNumber-1]
-            totalPriceLabel.text = "$" + String(format: "%.2f", orderTotal)
-        }
-    }
-    
-    @IBAction func applyTipPressed(_ sender: UIButton) {
-        
-        if(foodPictureView.image != nil){
-            let tipInt = Double(tipInputField.text!) ?? 0
-            let orderTotalTipped = orderTotal + orderTotal*(Double(tipInt)/100.00)
-            totalPriceLabel.text = "$" + String(format: "%.2f", orderTotalTipped)
-        }
-    }
-    
-    @IBAction func clearOrdersAddedButton(_ sender: UIButton) {
-        orderList.removeAll()
-        orderTotal = 0.0
-        orderTotalWithTip = 0.0
-        totalPriceLabel.text = "$" + String(format: "%.2f", orderTotal)
-        tipInputField.text = ""
-    }
-    
-    
+
     
     
 }
